bcc331b0b5030116b8e94014fd0f12a837be583b
Tomasz Graczyk <tgraczyk86@gmail.com>
2024-04-23 17:26:59 +0200

    Kalkulator

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import logging
 
 def calc(wybor, num1, num2):
     dict = {
-        "1": lambda: num1 + num2,
-        "2": lambda: num1 - num2,
-        "3": lambda: num1 * num2,
-        "4": lambda: num1 / num2,
-        "5": lambda: num1 ** num2,
+        "+": lambda: num1 + num2,
+        "-": lambda: num1 - num2,
+        "*": lambda: num1 * num2,
+        "/": lambda: num1 / num2,
+        "**": lambda: num1 ** num2,
         }.get(wybor, lambda : None)()
     return dict
 
-print("Wybierz operację: 1. Dodawanie ,2. Odejmowanie ,3. Mnożenie ,4. Dzielenie ,5. Potęgowanie ")
+print("Wybierz operację: +. Dodawanie ,-. Odejmowanie ,*. Mnożenie ,/. Dzielenie ,**. Potęgowanie ")
 wybor = input()
 num1 = float(input("Podaj pierwszy składnik: "))
 num2 = float(input("Podaj drugi składnik: "))
 
 if __name__ == "__main__":
-    print("Wynik działania to: ", calc(wybor, num1, num2))
+    print(f"Wynik: {num1} {wybor} {num2} =", calc(wybor, num1, num2),)
